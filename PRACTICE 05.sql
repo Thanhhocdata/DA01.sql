1dca076dde17da91aa227b4298b97bb8ed97f36c
Thanhhocdata <vuducthanh090503@gmail.com>
2025-01-13 17:22:38 +0700

    Create PRACTICE 05.sql

---
+++ PRACTICE 05.sql
@@ -0,0 +1,44 @@
+EX1
+SELECT b.CONTINENT, 
+Floor(AVG(a.population))
+FROM CITY AS a
+LEFT JOIN COUNTRY AS b
+ON a.COUNTRYCODE = b.CODE
+WHERE b.CONTINENT IS NOT NULL
+GROUP BY b.CONTINENT
+EX2
+SELECT
+ROUND(
+CAST (SUM(
+CASE
+WHEN signup_action = 'Confirmed' THEN 1 ELSE 0
+END) AS DECIMAL (10,2))
+/ CAST(COUNT(signup_action) AS DECIMAL (10,2)),2)
+AS signup_action
+FROM emails AS a
+LEFT JOIN texts AS b
+ON a.email_id =b.email_id
+EX3
+SELECT 
+a.age_bucket,
+ROUND(
+CAST(
+SUM(
+CASE
+WHEN b.activity_type = 'send' THEN time_spent ELSE 0
+END) AS DECIMAL (10,2))/ CAST(SUM(time_spent) AS DECIMAL (10,2))
+*100,2)
+AS send_perc,
+ROUND(
+CAST(
+SUM(
+CASE
+WHEN b.activity_type = 'open' THEN time_spent ELSE 0
+END) AS DECIMAL (10,2))/ CAST(SUM(time_spent) AS DECIMAL (10,2))
+*100,2)
+AS open_perc
+FROM age_breakdown AS a
+JOIN activities AS b 
+ON a.user_id = b.user_id
+WHERE activity_type IN ('open','send')
+GROUP BY age_bucket
