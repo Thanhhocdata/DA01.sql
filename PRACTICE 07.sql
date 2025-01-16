c268d580d0b026553e337375eaa98e1970d00b5a
Thanhhocdata <vuducthanh090503@gmail.com>
2025-01-16 15:18:27 +0700

    Create PRACTICE 07.sql

---
+++ PRACTICE 07.sql
@@ -0,0 +1,10 @@
+EX1
+SELECT 
+EXTRACT(YEAR FROM transaction_date) AS year,
+product_id,
+spend AS curr_year_spend,
+LAG(spend) OVER (PARTITION BY product_id ORDER BY transaction_date) AS prev_year_spend,
+ROUND((spend-LAG(spend) OVER (PARTITION BY product_id ORDER BY transaction_date)) / LAG(spend) OVER (PARTITION BY product_id ORDER BY transaction_date) *100,2) AS yoy_rate
+FROM user_transactions 
+EX2
+
