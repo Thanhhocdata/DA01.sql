a5666005454aba24386d3cb8414ac4f086e8bafd
Thanhhocdata <vuducthanh090503@gmail.com>
2025-01-17 18:02:58 +0700

    Update PRACTICE 08.sql

--- PRACTICE 08.sql
+++ PRACTICE 08.sql
@@ -10,14 +10,40 @@ ELSE 0 END)*1.00 /COUNT (*)*1.00 *100,2) AS immediate_percentage
 FROM a
 WHERE nth_purchase =1
 EX2
+--nth_log_in
 WITH a AS
 (SELECT player_id,event_date,
-LEAD (event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS next_event_date
-FROM Activity) ,
-b as (SELECT a.player_id,
- EXTRACT(YEAR FROM next_event_date)*365 +EXTRACT(MONTH FROM next_event_date)*30 +EXTRACT(DAY FROM next_event_date)-EXTRACT(YEAR FROM event_date)*365 -EXTRACT(MONTH FROM event_date)*30-EXTRACT(DAY FROM event_date) AS fraction 
-FROM a)
+RANK () OVER (PARTITION BY player_id ORDER BY event_date) AS nth_log_in
+FROM Activity),
+--the_first_log_in
+b1 AS
+(SELECT a.player_id,a.event_date AS the_first_event_date
+FROM  a
+WHERE nth_log_in =1),
+--the_second_log_in
+b2 AS 
+(SELECT a.player_id,a.event_date AS the_next_event_date
+FROM  a
+WHERE nth_log_in =2)
 SELECT 
-ROUND((SELECT COUNT (player_id ) FROM b WHERE fraction =1)*1.00/
-COUNT (DISTINCT b.player_id)*1.00,2) AS fraction 
+ROUND((SELECT COUNT(*)
+FROM b1
+JOIN b2
+ON b1.player_id =b2.player_id 
+WHERE b2.the_next_event_date-b1.the_first_event_date =1)*1.00
+/count (DISTINCT player_id)*1.00,2) AS fraction  
+FROM Activity
+EX3
+WITH b AS
+(SELECT id,student,
+CASE 
+WHEN id%2=0 then id-1
+WHEN id%2=1 then id+1
+END AS change_seat
+FROM seat)
+select b.id,
+COALESCE(a.student,b.student) AS student 
 FROM b
+LEFT JOIN Seat AS a
+ON a.id=b.change_seat 
+
