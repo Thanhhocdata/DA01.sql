9d24319ed5040033579ab2fcc61e7c34dc8d5f52
Thanhhocdata <vuducthanh090503@gmail.com>
2025-01-17 13:19:40 +0700

    Update PRACTICE 08.sql

--- PRACTICE 08.sql
+++ PRACTICE 08.sql
@@ -9,3 +9,15 @@ WHEN a.order_date =a.customer_pref_delivery_date THEN 1
 ELSE 0 END)*1.00 /COUNT (*)*1.00 *100,2) AS immediate_percentage 
 FROM a
 WHERE nth_purchase =1
+EX2
+WITH a AS
+(SELECT player_id,event_date,
+LEAD (event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS next_event_date
+FROM Activity) ,
+b as (SELECT a.player_id,
+ EXTRACT(YEAR FROM next_event_date)*365 +EXTRACT(MONTH FROM next_event_date)*30 +EXTRACT(DAY FROM next_event_date)-EXTRACT(YEAR FROM event_date)*365 -EXTRACT(MONTH FROM event_date)*30-EXTRACT(DAY FROM event_date) AS fraction 
+FROM a)
+SELECT 
+ROUND((SELECT COUNT (player_id ) FROM b WHERE fraction =1)*1.00/
+COUNT (DISTINCT b.player_id)*1.00,2) AS fraction 
+FROM b
