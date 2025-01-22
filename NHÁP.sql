fdb5c85902c2f701fdc7457db7a14c11acd7202e
Thanhhocdata <vuducthanh090503@gmail.com>
2025-01-22 11:28:39 +0700

    Update NHÁP.sql

--- NHÁP.sql
+++ NHÁP.sql
@@ -1,11 +1,7 @@
-NHÁP Phân loại trước group sau
-SELECT
-CASE
-WHEN length > 120 then 'long'
-WHEN length BETWEEN 60 AND 120  then 'medium'
-ELSE 'short'
-END AS category,
-CASE
-WHEN rating = 'R' THEN 1 ELSE 0
-END AS r
-FROM film
+WITH cte1 AS
+(SELECT 
+PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY quantityordered) - 1.5*(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY quantityordered) -PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY quantityordered)) AS MIN,
+PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY quantityordered) +1.5*(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY quantityordered) -PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY quantityordered)) AS MAX
+FROM public.sales_dataset_rfm_prj)
+SELECT * FROM public.sales_dataset_rfm_prj
+WHERE quantityordered <= (SELECT cte1.min from cte1) OR  quantityordered >= (SELECT cte1.max from cte1)
