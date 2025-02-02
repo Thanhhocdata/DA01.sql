402594f343cc2fea271cea9bb841c2fb3f9e2f1c
Thanhhocdata <vuducthanh090503@gmail.com>
2025-02-02 16:00:25 +0700

    Update PROJECT 03.sql

--- PROJECT 03.sql
+++ PROJECT 03.sql
@@ -33,3 +33,24 @@ SUM(sales) AS revenue
 FROM  public.sales_dataset_rfm_prj_clean
 group by YEAR_ID,productline) AS t
 EX5
+WITH customer_rfm AS
+(SELECT customername,
+current_date-max(orderdate) AS R,
+COUNT(DISTINCT ordernumber) AS F,
+sum(sales) AS M
+FROM public.sales_dataset_rfm_prj_clean
+GROUP BY customername),
+rfm_score AS
+(SELECT customername,
+ntile(5) OVER (ORDER BY R DESC) AS R_score,
+ntile(5) OVER (ORDER BY F DESC) AS F_score,
+ntile(5) OVER (ORDER BY M DESC) AS M_score
+FROM customer_rfm)
+SELECT a.customername,b.segment
+FROM 
+(SELECT customername,
+CAST(R_score AS VARCHAR)||CAST(F_score AS VARCHAR)||CAST(M_score AS VARCHAR) AS total_rfm_score
+FROM rfm_score) as a
+JOIN public.segment_score AS b
+ON a.total_rfm_score=b.scores
+WHERE b.segment IN ('Champions')
